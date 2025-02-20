be8f2ce7b2302060fdff2680b76a82f8e7ea359f
Jamie Li <lijamie@gmail.com>
2025-02-20 10:31:09 -0800

    Remove unused jwt

--- src/pages/TxnInfo.tsx
+++ src/pages/TxnInfo.tsx
@@ -3,18 +3,12 @@ import { Heading2 } from "@stellar/design-system";
 import { fetchTransaction } from "helpers/fetchTransaction";
 import TxnInfoBlock from "../components/TxnInfoBlock";
 
-export const TxnInfo = ({
-  txnId,
-  jwt,
-}: {
-  txnId: string | null;
-  jwt: string;
-}) => {
+export const TxnInfo = ({ txnId }: { txnId: string | null }) => {
   const [info, setInfo] = useState<any>(null);
 
   useEffect(() => {
     if (txnId) pollTransaction(txnId);
-  }, [txnId, jwt]);
+  }, [txnId]);
 
   const pollTransaction = async (txnId: string) => {
     const txnResponse = await fetchTransaction(txnId);
